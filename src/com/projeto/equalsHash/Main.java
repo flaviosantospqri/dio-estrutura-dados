2d605404ee8bc5e26e66efcc8f95be39a578cefa
Flavio Santos <flavio.pasantos@outlook.com>
2022-12-29 21:11:32 -0300

    adição de conteudo de hash

---
+++ src/com/projeto/equalsHash/Main.java
@@ -0,0 +1,19 @@
+package com.projeto.equalsHash;
+
+import java.util.ArrayList;
+import java.util.List;
+import  java.util.Stack;
+public class Main {
+
+    public static void main(String[] args) {
+        Stack<Carro> carroStack = new Stack<>();
+
+        carroStack.push(new Carro("Ford"));
+        carroStack.push(new Carro("Fiat"));
+        carroStack.push(new Carro("Vw"));
+        carroStack.push(new Carro("chev"));
+
+
+
+    }
+}
---
+++ src/com/projeto/equalsHash/Stack.java
@@ -0,0 +1,4 @@
+package com.projeto.equalsHash;
+
+public class Stack {
+}
